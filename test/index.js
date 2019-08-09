cc2ee606c99a4a95af017179db3ca1012bb7deb3
Will Harris-Braun <willharrisbraun@gmail.com>
2019-08-09 13:31:13 -0400

    Add tests

--- test/index.js
+++ test/index.js
@@ -27,14 +27,57 @@ const diorama = new Diorama({
 
 diorama.registerScenario("Can create a new game", async (s, t, {alice, bob}) => {
 
-  const create_game_result = await alice.call('main', 'create_game', {
+  // 1 - Alice can create game
+  const create_game_result = await alice.callSync('main', 'create_game', {
     opponent: bob.agentId,
     timestamp: 0
   })
-
   console.log(create_game_result)
-
   t.equal(create_game_result.Ok.length, 46)
+
+  // 2 - Bob can make first move, creating token
+  const move_1_result = await bob.callSync('main', 'make_move', {
+    new_move: {
+      game: create_game_result.Ok,
+      move_type: { CreateToken: { distance: 4 } },
+      timestamp: 1
+    }
+  })
+  console.log(move_1_result)
+  t.equal(move_1_result.Err, undefined)
+
+  // 3 - Alice then can't move because it's Bob's turn due to rosette
+  const move_2_result = await alice.callSync('main', 'make_move', {
+    new_move: {
+      game: create_game_result.Ok,
+      move_type: { CreateToken: { distance: 3 } },
+      timestamp: 2
+    }
+  })
+  console.log(move_2_result)
+  t.equal(move_2_result.Ok, undefined)
+
+  // 4 - Bob moves again
+  const move_3_result = await bob.callSync('main', 'make_move', {
+    new_move: {
+      game: create_game_result.Ok,
+      move_type: { CreateToken: { distance: 3 } },
+      timestamp: 3
+    }
+  })
+  console.log(move_3_result)
+  t.equal(move_3_result.Err, undefined)
+
+  // 5 - Now Alice can move
+  const move_4_result = await alice.callSync('main', 'make_move', {
+    new_move: {
+      game: create_game_result.Ok,
+      move_type: { CreateToken: { distance: 2 } },
+      timestamp: 4
+    }
+  })
+  console.log(move_4_result)
+  t.equal(move_4_result.Err, undefined)
 
 })
 
